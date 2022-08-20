200e03c002dcbe0da52b4ca46896b18203b5c5f4
hakkeempa <abdulhakkeempa2002@gmail.com>
2022-08-20 13:48:35 +0530

    preloader logo added

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,14 +183,20 @@
 })()
 
 
-content = document.querySelector(".pre-loader")
-loader = document.querySelector(".loader")
-window.addEventListener("load",function(){
-  setTimeout(myFunction, 1500)
-})
+// content = document.querySelector(".pre-loader")
+// loader = document.querySelector(".loader")
+// window.addEventListener("load",function(){
+//   setTimeout(myFunction, 1500)
+// })
 
-function myFunction(){
-  loader.style.opacity = 0.5
-  loader.style.display = "none";
-  content.style.visibility = "visible";
-}
+// function myFunction(){
+//   loader.style.opacity = 0.5
+//   loader.style.opacity = 0.3
+//   loader.style.visibility = "hidden";
+//   content.style.visibility = "visible";
+// }
+
+$(window).on("load",function(){
+  $(".pre-loader-img").fadeOut(2500);
+  setTimeout( function(){$(".pre-loader").css("visibility","visible");},2500);
+});
